896548abd11d49c23689633b3faa74adcdc6a914
tashchuk23 <tashchuk23@up.edu>
2021-09-30 13:13:18 -0700

    Initial commit - created GUI with title and 4 buttons in a gridlayout that can respond to clicks by changing a textView

---
+++ app/src/main/java/com/example/puzzlehw2/ButtonController.java
@@ -0,0 +1,17 @@
+package com.example.puzzlehw2;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class ButtonController implements View.OnClickListener{
+
+    public ButtonController(Button buttonObj){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/puzzlehw2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.puzzlehw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //create a textView that says which button was clicked
+        textView = findViewById(R.id.bText);
+
+        //create the buttons
+        Button b1 = (Button) findViewById(R.id.b1);
+        Button b2 = (Button) findViewById(R.id.b2);
+        Button b3 = (Button) findViewById(R.id.b3);
+        Button b4 = (Button) findViewById(R.id.b4);
+
+        //setOnClickListeners for all buttons
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+
+
+        //ButtonController controller = new ButtonController(b);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            //testing out how to register clicks for multiple buttons
+            //this sets the text to whatever the clicked button's text is
+            case R.id.b1:
+                textView.setText(R.string._1);
+                break;
+            case R.id.b2:
+                textView.setText(R.string._2);
+                break;
+            case R.id.b3:
+                textView.setText(R.string._3);
+                break;
+            case R.id.b4:
+                textView.setText(R.string._4);
+                break;
+        }
+    }
+}
